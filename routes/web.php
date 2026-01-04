02f0e9670b5f1776b419ad4da00ac9b10f7f0088
Taha Karami <tahakarami2088@gamil.com>
2026-01-04 14:20:30 +0330

    feat(web): add Front.Web Template to project

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+
+
+Route::get('admin/' , \App\Livewire\Admin\Panel\Index::class)->name('panel');
+Route::get('admin/user_create' , \App\Livewire\Admin\User\UserCreate::class)->name('user.create');
+Route::get('admin/users' , \App\Livewire\Admin\User\UserList::class)->name('users');
+
