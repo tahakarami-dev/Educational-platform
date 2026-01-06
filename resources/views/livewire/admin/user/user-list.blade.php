e10f272e5fc186eafcc231e76fac207d57e91b7f
Taha Karami <tahakarami2088@gamil.com>
2026-01-06 10:51:45 +0330

    fix:Optimization of requests, updating of user list and addition of new users

--- resources/views/livewire/admin/user/user-list.blade.php
+++ resources/views/livewire/admin/user/user-list.blade.php
@@ -1,117 +1,113 @@
 <main class="main-content">
     <div class="card">
         <div class="card-body">
-            <div class="card">
-                <div class="card-body">
-                    <div class="container">
-                        <h6 class="card-title">ایجاد کاربر</h6>
-                        @if(session()->has('message'))
-                            <div class="alert alert-info">
-                                {{session('message')}}
-                            </div>
-                        @endif
 
-                        <form wire:submit="saveuser">
-                            <div class="form-group row">
-                                <label  class="col-sm-2 col-form-label">نام و نام خانوادگی</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control text-left"  dir="rtl" name="name" wire:model="name">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label  class="col-sm-2 col-form-label">ایمیل</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control text-left" dir="rtl" name="email" wire:model="email" >
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label  class="col-sm-2 col-form-label">موبایل</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control text-left"  dir="rtl" name="mobile" wire:model="mobile">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label  class="col-sm-2 col-form-label">پسورد</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control text-left" dir="rtl" name="password" wire:model="password">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label" for="file"> آپلود عکس </label>
-                                <input  class="col-sm-10" type="file" class="form-control-file" id="file" wire:model="image">
-                            </div>
-                            <div class="form-group row">
-                                <button type="submit" class="btn btn-success btn-uppercase">
-                                    <i class="ti-check-box m-r-5"></i> ذخیره
-                                </button>
-
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
+            {{-- Create User --}}
+            <div class="card mb-4">
                 <div class="card-body">
-                    <div class="table overflow-auto" tabindex="8">
+                    <h6 class="card-title">ایجاد کاربر</h6>
+
+                    @if(session()->has('message'))
+                        <div class="alert alert-success">
+                            {{ session('message') }}
+                        </div>
+                    @endif
+
+                    <form wire:submit.prevent="saveUser">
                         <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">عنوان جستجو</label>
+                            <label class="col-sm-2 col-form-label">نام و نام خانوادگی</label>
                             <div class="col-sm-10">
-                                <input  type="text" class="form-control text-left" dir="rtl" wire:model.live="search">
+                                <input type="text" class="form-control" wire:model.defer="name">
+                                @error('name') <small class="text-danger">{{ $message }}</small> @enderror
                             </div>
                         </div>
-                        <table class="table table-striped table-hover">
-                            <thead class="thead-light">
-                            <tr>
-                                <th class="text-center align-middle text-primary">ردیف</th>
-                                <th class="text-center align-middle text-primary">عکس</th>
-                                <th class="text-center align-middle text-primary">نام و نام خانوادگی</th>
-                                <th class="text-center align-middle text-primary">ایمیل</th>
-                                <th class="text-center align-middle text-primary">موبایل</th>
-                                <th class="text-center align-middle text-primary">نقش های کاربر</th>
-                                <th class="text-center align-middle text-primary"> وضعیت</th>
-                                <th class="text-center align-middle text-primary">ویرایش</th>
-                                <th class="text-center align-middle text-primary">تاریخ ایجاد</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($users as $index=>$user)
 
-                            <tr>
-                                <td class="text-center align-middle">{{$user->id}}</td>
-                                <td class="text-center align-middle">
-                                    <figure class="avatar avatar">
-                                        <img src="" class="rounded-circle" alt="image">
-                                    </figure>
-                                </td>
-                                <td class="text-center align-middle">{{$user->name}}</td>
-                                <td class="text-center align-middle">{{$user->email}}</td>
-                                <td class="text-center align-middle">{{$user->mobile}}</td>
-                                <td class="text-center align-middle">
-                                    <a class="btn btn-outline-info" href="#">
-                                        نقش های کاربر
-                                    </a>
-                                </td>
-                                <td class="text-center align-middle">
-                                    <span class="cursor-pointer badge badge-success">فعال</span>
-                                </td>
-                                <td class="text-center align-middle">
-                                    <a class="btn btn-outline-info" href="#">
-                                        ویرایش
-                                    </a>
-                                </td>
-                                <td class="text-center align-middle"></td>
-                            </tr>
-                            @endforeach
-                        </table>
-                        <div style="margin: 40px !important;"
-                             class="pagination pagination-rounded pagination-sm d-flex justify-content-center">
-                            {{$users->links()}}
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">ایمیل</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" wire:model.defer="email">
+                                @error('email') <small class="text-danger">{{ $message }}</small> @enderror
+                            </div>
                         </div>
-                    </div>
+
+
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">پسورد</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" wire:model.defer="password">
+                                @error('password') <small class="text-danger">{{ $message }}</small> @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">آپلود عکس</label>
+                            <div class="col-sm-10">
+                                <input type="file" wire:model="image">
+                                <div wire:loading wire:target="image" class="text-info mt-1">
+                                    در حال آپلود...
+                                </div>
+                                @error('image') <small class="text-danger">{{ $message }}</small> @enderror
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-success"
+                                wire:loading.attr="disabled">
+                            ذخیره
+                        </button>
+                    </form>
                 </div>
             </div>
+
+            {{-- Search --}}
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">جستجو</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" wire:model.live="search"
+                           placeholder="نام، ایمیل یا موبایل">
+                </div>
+            </div>
+
+            {{-- Users Table --}}
+            <div class="table-responsive">
+                <table class="table table-striped table-hover">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>عکس</th>
+                        <th>نام</th>
+                        <th>ایمیل</th>
+                        <th>موبایل</th>
+                        <th>وضعیت</th>
+                        <th>تاریخ ایجاد</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>
+                                <img width="40" height="40" class="rounded-circle"
+                                     src="{{ $user->image
+                                        ? asset('storage/users/'.$user->image)
+                                        : asset('admin/default-avatar.png') }}">
+                            </td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td></td>
+                            <td>
+                                <span class="badge badge-success">فعال</span>
+                            </td>
+                            <td>{{ $user->created_at->format('Y/m/d') }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+
+                <div class="d-flex justify-content-center mt-4">
+                    {{ $users->links() }}
+                </div>
+            </div>
+
         </div>
     </div>
 </main>
-
-
